3149fc57e414c7fe7f4b8b9b443bef4527405a40
HyCraftHD <hycrafthd@live.de>
2019-04-12 19:16:00 +0200

    Item groups (4)

---
+++ src/main/java/net/hycrafthd/tutorialmod/init/TutorialItemGroups.java
@@ -0,0 +1,9 @@
+package net.hycrafthd.tutorialmod.init;
+
+import net.hycrafthd.tutorialmod.itemgroup.TutorialItemGroup;
+
+public class TutorialItemGroups {
+
+	public static final TutorialItemGroup group = new TutorialItemGroup();
+
+}
---
+++ src/main/java/net/hycrafthd/tutorialmod/itemgroup/TutorialItemGroup.java
@@ -0,0 +1,21 @@
+package net.hycrafthd.tutorialmod.itemgroup;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class TutorialItemGroup extends ItemGroup {
+
+	public TutorialItemGroup() {
+		super("tutorial");
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public ItemStack createIcon() {
+		return new ItemStack(Blocks.CHISELED_RED_SANDSTONE);
+	}
+
+}
